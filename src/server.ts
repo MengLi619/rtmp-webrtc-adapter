323bc0d38076c8aea5787c1f21c255b9e9a1aa5a
Meng Li <Mengli619@github.com>
2020-09-16 18:27:20 +0800

    fix

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ interface Source {
 
 const PORT = process.env.PORT || 3000;
 const RTC_MIN_PORT = Number(process.env.RTC_MIN_PORT || 30000);
-const RTC_MAX_PORT = Number(process.env.RTC_MIN_PORT || 31000);
+const RTC_MAX_PORT = Number(process.env.RTC_MAX_PORT || 31000);
 const RTC_LISTEN_IP = process.env.RTC_LISTEN_IP || '127.0.0.1';
 
 let expressApp: express.Express;
